c2298087c5b892c4664dda6b6f3cb668338ac8a3
unknown <unknown@example.com>
2025-03-26 22:49:32 +0900

    update

--- src/app/services/g.service.ts
+++ src/app/services/g.service.ts
@@ -10,7 +10,7 @@ export type MultilingualPrompt = Record<Lang, string>;
 })
 export class GService {
 
-  version = 'v20250310';
+  version = 'v20250326';
 
   // ローディング中のHTTP通信数
   httpConnectCount: Subject<number> = new Subject<number>();
